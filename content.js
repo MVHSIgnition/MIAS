082416b8ca4d0645b961fb1b96b79bec6ad64846
TtonyxX <xyytonyxin@gmail.com>
2018-10-20 13:53:41 -0700

    Added color change and style sutff
    asdf

--- content.js
+++ content.js
@@ -1,5 +1,7 @@
 var div = document.createElement('div');
-
+var spanOne = document.createElement('span');
+var spanTwo = document.createElement('span');
+var spanThree = document.createElement('span');
 
 div.classList.add('mias_container');
 
@@ -10,7 +12,11 @@ else{
     document.body.appendChild(div);
 }
 
-div.innerHTML += 'Bias: ';
+spanOne.setAttribute("style", "font-weight: bold;");
+spanTwo.setAttribute("style", "font-weight: bold;");
+spanOne.innerHTML += 'Bias: ';
+div.appendChild(spanOne);
+
 xhr = new XMLHttpRequest();
 xhr2 = new XMLHttpRequest();
 var url = "http://localhost:8080/fakebox/check";
@@ -27,7 +33,18 @@ xhr.onreadystatechange = function () {
             if (xhr2.readyState == 4 && xhr2.status == 200) {
                 var json2 = JSON.parse(xhr2.responseText);
                 console.log(JSON.stringify(json2.content) + "\n\n" + JSON.stringify(json2.title));
-                div.innterHTML += json2.content.decision + " Score: " + json2.content.score;
+                spanThree.innerHTML += JSON.stringify(json2.content.decision);
+                if(Double.parseDouble(JSON.stringify(json2.content.score))<0.25) {
+                	spanThree.classList.add('high');
+                } else if(Double.parseDouble(JSON.stringify(json2.content.score))>0.7) {
+                	spanThree.classList.add('low');
+                } else {
+                	spanThree.classList.add('med');
+                }
+                div.appendChild(spanThree);
+                spanTwo.innerHTML += " Score: ";
+                div.appendChild(spanTwo);
+                div.innerHTML += Math.round(Math.round(Double.parseDouble(JSON.stringify(json2.content.score)))*100)/100;
             }
         }
         var data = JSON.stringify({'url':window.location.href,'title': json.title,'content': json.body});
