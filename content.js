ec6da630b970fbbda1a5ba9fd441430f876758f3
erikbZhang <erikbzhang@yahoo.com>
2018-10-20 17:29:20 -0700

    broken code?????????

--- content.js
+++ content.js
@@ -1,3 +1,27 @@
+/*
+var AylienNewsApi = 'aylien-news-api';
+
+var apiInstance = new AylienNewsApi.DefaultApi();
+
+// Configure API key authorization: app_id
+var app_id = apiInstance.apiClient.authentications['app_id'];
+app_id.apiKey = "f2ceabc2";
+
+// Configure API key authorization: app_key
+var app_key = apiInstance.apiClient.authentications['app_key'];
+app_key.apiKey = "4aa0715bddaf684e505187c09ed93777";
+
+var opts = {
+  'title': 'trump',
+  'sortBy': 'social_shares_count.facebook',
+  'language': ['en'],
+  'publishedAtStart': 'NOW-7DAYS',
+  'publishedAtEnd': 'NOW',
+  'entitiesBodyLinksDbpedia': [
+    'http://dbpedia.org/resource/Donald_Trump',
+    'http://dbpedia.org/resource/Hillary_Rodham_Clinton'
+  ]
+};*/
 var script = document.createElement('script');
 script.src = '//code.jquery.com/jquery-1.11.0.min.js';
 document.getElementsByTagName('head')[0].appendChild(script);
@@ -9,8 +33,18 @@ var spanThree = document.createElement('span');
 var spanFour = document.createElement('span');
 var drag = document.createElement('div');
 
-document.getElementByTagName("body")[0].appendChild(img);
-img.setAttribute('style', 'position: fixed; right: 0px; top: 50%;');
+
+
+var callback = function(error, data, response) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('API called successfully. Rate limit headers are as follows:');
+    console.log('X-RateLimit-Limit:', response.headers['x-ratelimit-limit']);
+    console.log('X-RateLimit-Remaining:', response.headers['x-ratelimit-remaining']);
+    console.log('X-RateLimit-Reset:', response.headers['x-ratelimit-reset']);
+  }
+};
 
 div.classList.add('mias_container');
 
@@ -28,7 +62,7 @@ var getHostName = function (url) {
       return null; // Invalid URL
     }
 }
-  
+
 var getPath = function (url) {
     try {
       return new URL(url).pathname;
@@ -38,14 +72,14 @@ var getPath = function (url) {
 }
 
 window.browser = chrome;
-  
+
 var getTabSource = function (url, cb) {
     browser.storage.local.get(['biases', 'sources'], function (items) {
       var hostName = getHostName(url);
       var path = getPath(url);
       var domainSources;
       var source;
-  
+
       var getDomainSource = function (domain) {
         domainSources = items.sources[domain];
         // check if value is an array or object
@@ -87,10 +121,10 @@ var getTabSource = function (url, cb) {
         }
         return source;
       }
-  
+
       if (items.biases !== undefined && items.sources !== undefined) {
         source = getDomainSource(hostName);
-  
+
         if (source !== undefined) {
           var bias = items.biases[source.bias];
         }
@@ -98,7 +132,7 @@ var getTabSource = function (url, cb) {
       cb(source, bias);
     });
 };
-  
+
 
 getTabSource(window.location.href, function(source, bias) {
     console.log(source, bias);
@@ -114,44 +148,41 @@ xhr = new XMLHttpRequest();
 xhr2 = new XMLHttpRequest();
 var url = "http://localhost:8080/fakebox/check";
 
-xhr.open("GET", 'https://document-parser-api.lateral.io/?url='+window.location.href, true);
-xhr.setRequestHeader("Content-type", "application/json");
-xhr.setRequestHeader('subscription-key', 'e45749374a35a4f62a6408650661b28b');
-xhr.onreadystatechange = function () {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-        var json = JSON.parse(xhr.responseText);
-        xhr2.open("POST", url, true);
-        xhr2.setRequestHeader("Content-type", "application/json");
-        xhr2.onreadystatechange = function () {
-            if (xhr2.readyState == 4 && xhr2.status == 200) {
-                var json2 = JSON.parse(xhr2.responseText);
-<<<<<<< HEAD
-                console.log(JSON.stringify(json2.content) + "\n\n" + JSON.stringify(json2.title) + "\n\n" + JSON.stringify(json2.keywords)+JSON.stringify(json2.domain.category));
-=======
-                console.log(JSON.stringify(json2.content) + "\n\n" + JSON.stringify(json2.title));
+    xhr.open("GET", 'https://document-parser-api.lateral.io/?url='+window.location.href, true);
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.setRequestHeader('subscription-key', 'e45749374a35a4f62a6408650661b28b');
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var json = JSON.parse(xhr.responseText);
+            xhr2.open("POST", url, true);
+            xhr2.setRequestHeader("Content-type", "application/json");
+            xhr2.onreadystatechange = function () {
+                if (xhr2.readyState == 4 && xhr2.status == 200) {
+                    var json2 = JSON.parse(xhr2.responseText);
+                    console.log(JSON.stringify(json2.content) + "\n\n" + JSON.stringify(json2.title) + "\n\n" + JSON.stringify(json2.keywords)+JSON.stringify(json2.domain.category));
 
->>>>>>> 11730cce1b6f25d2a2dba4695fbed63969fe2063
-                spanThree.innerHTML += JSON.stringify(json2.content.decision);
-                div.appendChild(spanThree);
-                spanTwo.innerHTML += " Score: ";
-                div.appendChild(spanTwo);
-                spanFour.innerHTML += Math.round(parseFloat(JSON.stringify(json2.content.score))*100)/100;
-                div.appendChild(spanFour);
-                if(parseFloat(JSON.stringify(json2.content.score))<0.25) {
-                	spanThree.setAttribute("style", "color: red");
-                	panThree.setAttribute("style", "color: red");
-                	console.log(spanThree.classList);
-                } else if(parseFloat(JSON.stringify(json2.content.score))>0.7) {
-                	spanThree.setAttribute("style", "color: green");
-                	spanThree.setAttribute("style", "color: green");
-                } else {
-                	spanThree.setAttribute("style", "color: yellow");
-                	spanThree.setAttribute("style", "color: yellow ");
+                    if (sourceDiv.innerHTML == '') {
+                        sourceDiv.innerHTML = '<p>Source: ' + json2.domain.domain + '<br/></p>';
+                    }
+                    sourceDiv.innerHTML += '<p>Category: ' + json2.domain.category + '</p>';
+
+                    spanThree.innerHTML += JSON.stringify(json2.content.decision);
+                    if(parseFloat(JSON.stringify(json2.content.score))<0.25) {
+                      spanThree.setAttribute("style", "color: green");
+                    } else if(parseFloat(JSON.stringify(json2.content.score))>0.7) {
+                      spanThree.setAttribute("style", "color: green");
+                    } else {
+                      spanThree.setAttribute("style", "color: yellow");
+                    }
+                    console.log(spanThree.classList);
+                    spanTwo.innerHTML += " Score: ";
+
+                    div.innerHTML += Math.round(parseFloat(JSON.stringify(json2.content.score))*100)/100;
+
                 }
             }
         }
         var data = JSON.stringify({'url':window.location.href,'title': json.title,'content': json.body});
         xhr2.send(data);
     }
-}
 xhr.send();
